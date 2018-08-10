fdda29d403b661b3f52c70eb2a10829c52ccc2b1
WafaaAyoubi <wafaa199711@gmail.com>
2018-08-10 18:16:09 +0300

    initial commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,62 @@
 import React, {Component} from 'react';
-import {TextInput, Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, TextInput, Platform, StyleSheet, Text, View} from 'react-native';
+
+const  num1 = Math.floor(Math.random() * 10) + 1;
+const num2 = Math.floor(Math.random() * 10) + 1;
+
 
 export default class Main extends Component<Props> {
    state = {
-       points: 0
+       points: 0,
+       userAns: null
      };
+
+     userAnswerOnChangeHandler = val => {
+        this.setState({
+         userAns: val
+       });
+     };
+
+    placeSubmitHandler = (ans) => {
+    if (this.state.userAns === ans) {
+      alert("True Answer");
+      this.setState(prevState =>{
+         return {
+           points: prevState.points + 2
+        };
+      });
+    }
+    else {
+      alert("Wrong Answer");
+      this.setState(prevState =>{
+        return {
+           points:  Math.floor(prevState.points / 2)
+       };
+      });
+}
+  };
   render() {
-     let num1 = Math.floor(Math.random() * 100) + 1;
-     let num2 = Math.floor(Math.random() * 100) + 1;
     return (
+
       <View style={styles.container}>
             <View style={styles.header}>
                  <Text style={styles.headerText}>- Math -</Text>
              </View>
              <View style={styles.mainView}>
                <Text style={styles.QuesText}>{num1} + {num2}</Text>
+               <Text style={styles.points}>{this.state.points}</Text>
              </View>
              <TextInput
                        style={styles.textInput}
+                       keyboardType = 'numeric'
                        placeholder='>Your Answer'
+                       onChangeText={this.userAnswerOnChangeHandler}
                        placeholderTextColor='white'
+                       value={this.state.userAns}
                        underlineColorAndroid='transparent'>
-                   </TextInput>
+            </TextInput>
+            <Button onPress={() => this.placeSubmitHandler((num1+num2).toString())}
+            title="Press Me" />
       </View>
     );
   }
@@ -44,10 +79,13 @@ const styles = StyleSheet.create({
          padding: 20
      },
      mainView: {
+        flex: 1,
        alignItems: 'center',
 
      },
      QuesText: {
+        flex: 1,
+        position: 'absolute', top: 100,
         fontSize: 30,
         padding: 20
      },
@@ -59,4 +97,10 @@ const styles = StyleSheet.create({
       borderTopWidth:2,
       borderTopColor: '#ededed'
    },
+   points: {
+      flex: 1,
+      position: 'absolute', top: 160,
+      fontSize: 40,
+      padding: 20
+   }
 });
